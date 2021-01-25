fix average meter

cil/lucir/codes/utils_incremental/train_eval.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from utils_pytorch import *
 from Utils import DataUtils
-from Utils import AverageMeter
+import AverageMeter as AverageMeter
 utils = DataUtils()
 
 #code for baseline1 : FT+standard distillation

cil/lucir/codes/utils_incremental/train_eval_MS.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 from utils_pytorch import *
 from Utils import DataUtils
-from Utils import AverageMeter
+import AverageMeter as AverageMeter
 utils = DataUtils()
 
 def get_old_scores_before_scale(self, inputs, outputs):